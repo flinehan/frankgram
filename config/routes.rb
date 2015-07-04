9e1ac32acbea769391c336e6e5789bd607410505
Frank Linehan <frankie.linehan@gmail.com>
2015-07-03 19:23:43 -0600

    added the paper clips gems to allow for picture uploads
    Signed-off-by: Frank Linehan <frankie.linehan@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,11 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  
+  
+  
+  resources:posts
+  root "posts#index"
+  
+  
 end
