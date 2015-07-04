cc43f55e9c5705257c6f52ece84cd6e22d7ad65d
Frank Linehan <frankie.linehan@gmail.com>
2015-07-04 11:51:41 -0600

    images are uploading and being displayed on the view.
    Signed-off-by: Frank Linehan <frankie.linehan@gmail.com>

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,14 @@
 class PostsController < ApplicationController
 	def index
+		@posts = Post.all
 	end
 	def new
-	  @post = Post.create
+	    @post = Post.create
 	end
 	def create
 		@post = Post.create(post_params)
+
+
 		redirect_to posts_path  
 	end
 
