57eecce3a0b06e0e2604bebbacd6d9d3f56ee279
Frank Linehan <frankie.linehan@gmail.com>
2015-07-04 11:02:54 -0600

    post controller allows for file uploads and redirects back to the index
    Signed-off-by: Frank Linehan <frankie.linehan@gmail.com>

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,5 +4,15 @@ class PostsController < ApplicationController
 	def new
 	  @post = Post.create
 	end
+	def create
+		@post = Post.create(post_params)
+		redirect_to posts_path  
+	end
+
+	private
+
+	def post_params  
+	  params.require(:post).permit(:image, :caption)
+	end  
 
 end
