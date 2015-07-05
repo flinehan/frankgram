706bb0dce01f53ccbc1c2aba172b381ae3e8696b
Frank Linehan <frankie.linehan@gmail.com>
2015-07-05 16:54:56 -0600

    adding the edit view and actions
    Signed-off-by: Frank Linehan <frankie.linehan@gmail.com>

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,36 @@
 class PostsController < ApplicationController
+	
+	#index get all posts
 	def index
 		@posts = Post.all
 	end
+	
+	#the new view
 	def new
+		#binds the post var to the view
 	    @post = Post.create
 	end
+
+	#view new view action
 	def create
 		@post = Post.create(post_params)
-
-
 		redirect_to posts_path  
+	end
+
+	#edit 'view'
+	def edit
+		#get the post we want to edit
+		@post = Post.find(params[:id])
+	end
+
+	#update is the view action
+	def update
+		#grab the post again
+		@post = Post.find(params[:id])
+		#update the post
+		@post.update(post_params)
+		#redirect to the post url for the post
+		redirect_to(post_path(@post))
 	end
 
 	def show
