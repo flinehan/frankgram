ada4e3f22f434301a849c93e2d8933afce4c9f90
Frank Linehan <frankie.linehan@gmail.com>
2015-07-05 18:23:45 -0600

    code clean up and and flashing of notifications to users
    Signed-off-by: Frank Linehan <frankie.linehan@gmail.com>

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-	
+	before_action :set_post, only: [:show, :edit, :update, :destroy]
 	#index get all posts
 	def index
 		@posts = Post.all
@@ -13,37 +13,43 @@ class PostsController < ApplicationController
 
 	#view new view action
 	def create
-		@post = Post.create(post_params)
-		redirect_to posts_path  
+		if @post = Post.create(post_params)
+		  flash[:success] = "Your post has been created!"
+		  redirect_to posts_path
+		else
+		  flash[:alert] = "Your new post couldn't be created!  Please check the form."
+		  render :new
+		end 
 	end
 
 	#edit 'view'
 	def edit
-		#get the post we want to edit
-		@post = Post.find(params[:id])
 	end
 
 	#update is the view action
 	def update
-		#grab the post again
-		@post = Post.find(params[:id])
-		#update the post
-		@post.update(post_params)
-		#redirect to the post url for the post
-		redirect_to(post_path(@post))
+	    if @post.update(post_params)
+	      flash[:success] = "Post updated."
+	      redirect_to posts_path
+	    else
+	      flash[:alert] = "Update failed.  Please check the form."
+	      render :edit
+	    end
 	end
 
 	def destroy
-		@post = Post.find(params[:id])
 		@post.destroy
 		redirect_to posts_path
 	end
 
 	def show
-		@post = Post.find(params[:id])
 	end
 
 	private
+
+	def set_post
+		@post = Post.find(params[:id])
+	end
 
 	def post_params  
 	  params.require(:post).permit(:image, :caption)
