cd90ba79c1123d579ec3fbd3fab2534ece0f0836
Frank Linehan <frankie.linehan@gmail.com>
2015-07-19 16:14:37 -0600

    creating realationship between users and their posts
    Signed-off-by: Frank Linehan <frankie.linehan@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :posts, dependent: :destroy
   validate :user_name, presence: true, length: {minimum: 4, maximum: 16}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150719212725) do
+ActiveRecord::Schema.define(version: 20150719220822) do
 
   create_table "posts", force: true do |t|
     t.string   "caption"
@@ -21,7 +21,10 @@ ActiveRecord::Schema.define(version: 20150719212725) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
