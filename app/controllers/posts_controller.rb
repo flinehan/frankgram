c665306ae24d7d016abd18a66213b62b40e0df09
Frank Linehan <frankie.linehan@gmail.com>
2015-07-19 16:41:31 -0600

    securing routes
    Signed-off-by: Frank Linehan <frankie.linehan@gmail.com>

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
 	before_action :authenticate_user!
 	before_action :set_post, only: [:show, :edit, :update, :destroy]
+	before_action :owned_post, only: [:edit, :update, :destroy]
+	
 	#index get all posts
 	def index
 		@posts = Post.all
@@ -9,12 +11,13 @@ class PostsController < ApplicationController
 	#the new view
 	def new
 		#binds the post var to the view
-	    @post = Post.create
+	    @post = current_user.posts.build
 	end
 
 	#view new view action
 	def create
-		if @post = Post.create(post_params)
+		@post = current_user.posts.build(post_params)
+		if @post.save
 		  flash[:success] = "Your post has been created!"
 		  redirect_to posts_path
 		else
@@ -55,5 +58,12 @@ class PostsController < ApplicationController
 	def post_params  
 	  params.require(:post).permit(:image, :caption)
 	end  
+
+	def owned_post
+		unless current_user == @post.user
+	    	flash[:alert] = "That is not your post."
+			redirect_to root_path
+		end
+	end
 
 end
